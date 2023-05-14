5e1f559f91365393c96dae0700accc4ade1a38f3
y00gi <yogendrajput0@gmail.com>
2023-05-14 13:46:50 +0530

    Adding new list of solutions -Pattern

---
+++ Patterns 1/Square Pattern.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+
+    int n;
+    cin >> n;
+
+    int i=0;
+
+    while(i<n){
+        int j = 0;
+        while(j<n){
+            cout << n;
+            j++;
+        }
+
+        i++;
+        cout << endl;
+    }
+  
+}
+
+
