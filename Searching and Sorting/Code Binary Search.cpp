6c9b3ebeb02ede78304896021a87f6e884cb1413
y00gi <yogendrajput0@gmail.com>
2023-06-21 14:27:43 +0530

    Adding Solution of Binary Search

---
+++ Searching and Sorting/Code Binary Search.cpp
@@ -0,0 +1,27 @@
+int search(vector<int>& nums, int target) {
+    // Write Your Code Here
+
+    int start = 0;
+    int end = nums.size()-1;
+
+    while(start<=end)
+    {
+        int mid = (start+end)/2;
+        
+        if(nums[mid] == target)
+        {
+            return mid;
+        }
+        else if(nums[mid]>target)
+        {
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+        }
+    }
+
+    return -1;
+
+}
