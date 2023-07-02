60f98ee3b79575f69adab95f85814791ff46ccc7
y00gi <yogendrajput0@gmail.com>
2023-07-02 16:12:15 +0530

    Adding Bubble Sort

---
+++ Searching and Sorting/Code Bubble Sort.cpp
@@ -0,0 +1,16 @@
+void bubbleSort(int arr[], int n) {
+    // Write Your Code Here
+
+    for(int i=0; i<n; i++)
+    {
+        
+        for(int j =0; j<n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+}
