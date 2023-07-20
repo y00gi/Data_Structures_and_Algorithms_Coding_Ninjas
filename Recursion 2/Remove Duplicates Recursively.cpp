83c98f2ca1758fcb8cc39acca687da85809dfdd2
y00gi <yogendrajput0@gmail.com>
2023-07-20 15:51:13 +0530

    Adding some solution of recursion 2

---
+++ Recursion 2/Remove Duplicates Recursively.cpp
@@ -0,0 +1,23 @@
+
+void removeConsecutiveDuplicates(char *input) {
+	/* Don't write main().
+	* Don't read input, it is passed as function argument.    
+	* Change in the given string itself.
+	* No need to return or print anything
+	* Taking input and printing output is handled automatically.
+	*/
+
+    if(input[0]=='\0') return;
+
+    if(input[0]==input[1])
+    {
+        for(int i=0; input[i]!='\0'; i++)
+        {
+            input[i] = input[i+1];
+        }
+        removeConsecutiveDuplicates(input);
+    }
+
+    removeConsecutiveDuplicates(input+1);
+
+}
