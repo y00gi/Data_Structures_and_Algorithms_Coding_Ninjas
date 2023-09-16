ac7be747152a292f6b64d50e21907d626fa07fc8
y00gi <yogendrajput0@gmail.com>
2023-09-16 17:45:11 +0530

    adding delete node

---
+++ Linked List 1/Delete node .cpp
@@ -0,0 +1,39 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *deleteNode(Node *head, int pos)
+{
+    // Write your code here.
+    if(head == NULL) return head;
+
+    if(pos == 0) return head->next;
+
+    Node *curr = head;
+    int currPos = 0;
+
+    while(curr != NULL && currPos < pos-1)
+    {
+        curr = curr -> next;
+        currPos++;
+    }
+
+    if(curr == NULL || curr -> next == NULL) return head;
+
+    curr -> next = curr -> next -> next;
+    return head;
+}
